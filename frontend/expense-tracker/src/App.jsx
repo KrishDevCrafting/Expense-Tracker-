8f2fc1d59b77a42ea7f6a05745531447db141f10
KrishDevCrafting <krishkumer150@gmail.com>
2025-07-12 22:50:32 +0530

    commit

--- frontend/expense-tracker/src/App.jsx
+++ frontend/expense-tracker/src/App.jsx
@@ -1,21 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <div>
-<p>
-  Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis magnam sequi animi in quas officiis deleniti neque impedit et eos!
-</p>
-
-      </div>
+        <h1 style={{ color: "red" }}>F1</h1>
+        <p>
+          F1 is the most risky and expensive sport where the racer driver their
+          car around 300km/hr speed! Formula 1 teams invest millions in
+          technology and safety to compete at the highest level. The sport is
+          known for its thrilling races, advanced engineering, and passionate
+          fanbase. Drivers require exceptional skill, quick reflexes, and
+          intense physical fitness to handle the demands of racing at such high
+          speeds.
+        </p>
+      </div>{" "}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
