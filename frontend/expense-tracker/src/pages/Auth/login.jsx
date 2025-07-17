b9cf5a016c577aa1ea5b7b7dd86ab89efe61fd46
KrishDevCrafting <krishkumer150@gmail.com>
2025-07-17 23:14:26 +0530

    props

--- frontend/expense-tracker/src/pages/Auth/login.jsx
+++ frontend/expense-tracker/src/pages/Auth/login.jsx
@@ -1,7 +1,19 @@
-import React from 'react'
-
-export default function Login() {
+import React from "react";
+import AuthLayout from "../../components/layout/AuthLayout";
+const Login = () => {
   return (
-    <div>login</div>
-  )
-}
+    <>
+      <AuthLayout>
+        
+        <div className="lg:w-[70%] md:h-full flex flex-col justify-center">
+          <h3 className="text-xl font-semibold">Welcome Back</h3>
+          <p className="text-xs text-slate-700 mt-[5px] md-6">
+            please enter your details to log in
+          </p>
+        </div>
+      </AuthLayout>
+    </>
+  );
+};
+
+export default Login;
