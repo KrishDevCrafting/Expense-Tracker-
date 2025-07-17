09f4ba31765aa3752ed6b762e6184baeba793150
KrishDevCrafting <krishkumer150@gmail.com>
2025-07-17 22:01:33 +0530

    tailwind-config-done

--- frontend/expense-tracker/src/App.jsx
+++ frontend/expense-tracker/src/App.jsx
@@ -7,12 +7,12 @@ function App() {
     <>
       <div>
         <h1 style={{ color: "red" }}>F1</h1>
-        <p>
+        <p className="text-2xl">
           F1 is the most risky and expensive sport where the racer driver their
           car around 300km/hr speed! Formula 1 teams invest millions in
           technology and safety to compete at the highest level. The sport is
           known for its thrilling races, advanced engineering, and passionate
-          fanbase. Drivers require exceptional skill, quick reflexes, and                                     
+          fanbase. Drivers require exceptional skill, quick reflexes, and
           intense physical fitness to handle the demands of racing at such high
           speeds.
         </p>
