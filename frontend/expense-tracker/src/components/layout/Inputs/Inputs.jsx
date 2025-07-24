c8e8a19a7ca0b0f9981e9d2306067cd8e3b5b392
KrishDevCrafting <krishkumer150@gmail.com>
2025-07-24 23:08:35 +0530

    updated

--- frontend/expense-tracker/src/components/layout/Inputs/Inputs.jsx
+++ frontend/expense-tracker/src/components/layout/Inputs/Inputs.jsx
@@ -1,7 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
+import { FaRegEye, FaRegEyeSlash } from "react-icons/fa6";
+const Input = ({ value, onChange, placeholder, label, type }) => {
+  const [showPassword, setShowPassword] = useState(false);
 
-const Input = () => {
-  return <div>Inputs</div>;
+  const toggleShowPassword = () => {
+    setShowPassword(!showPassword);
+  };
+
+  return (
+    <div>
+      <label className="text-[13px] text-slate-800">{label}</label>
+      <div>
+        <input
+          type={
+            type == "password" ? (showPassword ? "test" : "password") : type
+          }
+          placeholder="placeholder"
+          className="w-full bg- bg-transparent outline-none"
+          value={value}
+          onChange={(e) => onchange(e)}
+        />
+        {type === "password" && (
+          <>
+            {showPassword ? (
+              <FaRegEye
+                size={22}
+                className="text-primary cursor-pointer"
+                onClick={() => toggleShowPassword()}
+              />
+            ) : (
+              <FaRegEyeSlash
+                size={22}
+                className="text-slate-400 cursor-pointer"
+                onClick={() => toggleShowPassword()}
+              />
+            )}
+          </>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default Input;
