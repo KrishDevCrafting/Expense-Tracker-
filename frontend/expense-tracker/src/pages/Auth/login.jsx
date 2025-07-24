1054c2b038c1755675fa1eb6790eebfd5fc93648
KrishDevCrafting <krishkumer150@gmail.com>
2025-07-24 22:38:21 +0530

    update

--- frontend/expense-tracker/src/pages/Auth/login.jsx
+++ frontend/expense-tracker/src/pages/Auth/login.jsx
@@ -1,15 +1,31 @@
 import React from "react";
 import AuthLayout from "../../components/layout/AuthLayout";
+import { useNavigate } from "react-router-dom";
+import { LuTarget } from "react-icons/lu";
 const Login = () => {
+  const [email, setEmail] = React.useState("");
+  const [password, setPassword] = React.useState("");
+  const [error, setError] = React.useState(null);
+  const navigate = useNavigate();
   return (
+    //  Handle Login Foprm Submit!!
     <>
       <AuthLayout>
-        
         <div className="lg:w-[70%] md:h-full flex flex-col justify-center">
           <h3 className="text-xl font-semibold">Welcome Back</h3>
           <p className="text-xs text-slate-700 mt-[5px] md-6">
             please enter your details to log in
           </p>
+
+          <form action="" onSubmit={handleleLogin}>
+            <Input
+              value={email}
+              onChange={({ target }) => setEmail(target.value)}
+              label="Email Address"
+              placeholder="Enter your email"
+              type="email"
+            />
+          </form>
         </div>
       </AuthLayout>
     </>
