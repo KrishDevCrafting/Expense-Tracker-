82999dc8bed6d2fac1b00cb1330ae5308defdda8
KrishDevCrafting <krishkumer150@gmail.com>
2025-07-24 22:08:38 +0530

    ui

--- frontend/expense-tracker/src/components/layout/AuthLayout.jsx
+++ frontend/expense-tracker/src/components/layout/AuthLayout.jsx
@@ -45,18 +45,17 @@ const StatsInfoCard = ({ icon, lebel, value, color }) => {
   return (
     <>
       <div
-        className="flex gap-6 bg-white p-4 rounded-xl shadow-rd shadow-purple-400/100
-      border-gray-200/50 z-10"
+        className="flex gap-6 bg-white p-4 rounded-xl shadow-md shadow-purple-400/100 border border-gray-200/50 z-10"
       >
         <div
-          className={`w-15 h-1 flex item-center justify-center text-[26px] text-white ${color} rounded-full drop-shadow-xl`}
+          className={`w-12 h-12 flex items-center justify-center text-[26px] text-white ${color} rounded-full drop-shadow-xl`}
         >
           {icon}
         </div>
-      </div>
-      <div>
-        <h6 className="text-xs text-gray-500 mb-1">{lebel}</h6>
-        <span className="text-[20px]">${value}</span>
+        <div>
+          <h6 className="text-xs text-gray-500 mb-1">{lebel}</h6>
+          <span className="text-[20px]">${value}</span>
+        </div>
       </div>
     </>
   );
