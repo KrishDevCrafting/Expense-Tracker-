2e2729ad0402cdea9563d27a3e8ca32e0a3938f4
KrishDevCrafting <krishkumer150@gmail.com>
2025-08-28 14:55:04 +0530

    authRoutes-added post-upload

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -5,6 +5,7 @@ const {
   loginUser,
   getUserInfo,
 } = require("../controller/authControler");
+const upload = require("../middleware/uploadMiddleware");
 
 const router = express.Router();
 
@@ -13,5 +14,16 @@ router.post("/register", registerUser);
 router.post("/login", loginUser);
 
 router.get("/getUser", protect, getUserInfo);
+router.post("/upload-image", upload.single("image"), (req, res) => {
+  if (!req.file) {
+    return res.status(400).json({
+      message: "No File uploaded",
+    });
+  }
+  const imageUrl = `${res.protocol}://${req.get("host")}/uploads/${
+    req.file.filename
+  }`;
 
+  res.status(200).json({ imageUrl });
+});
 module.exports = router;
