d7c90595be450aaf2f8ed977b6c21246bb7174df
KrishDevCrafting <krishkumer150@gmail.com>
2025-09-16 22:10:06 +0530

    updatad

--- frontend/expense-tracker/src/utils/uploadImage.js
+++ frontend/expense-tracker/src/utils/uploadImage.js
@@ -4,22 +4,25 @@ import axiosInstance from "./axios";
 
 const uploadImage = async (imageFile) => {
   const formData = new FormData();
-  formData.append("profileImage", imageFile); // Use correct key and value
 
+  // Append image file to form data
+  formData.append("image", imageFile);
   try {
     const response = await axiosInstance.post(
       API_PATHS.IMAGE.UPLOAD_IMAGE,
       formData,
       {
-        headers: {
+        header: {
           "Content-Type": "multipart/form-data",
         },
       }
     );
     return response.data;
+    // return Response data
   } catch (error) {
-    console.error("Error uploading the image:", error);
+    console.error("error uploading the image", error);
     throw error;
+    // Rethrow error for headling
   }
 };
 
