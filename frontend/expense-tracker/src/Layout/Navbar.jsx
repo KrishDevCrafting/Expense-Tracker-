f8d38d9c9974e459f2e7b380bcd22d5589bd42ed
KrishDevCrafting <krishkumer150@gmail.com>
2025-09-19 22:59:46 +0530

    sideMenu-updated

--- frontend/expense-tracker/src/Layout/Navbar.jsx
+++ frontend/expense-tracker/src/Layout/Navbar.jsx
@@ -6,26 +6,31 @@ export const Navbar = ({ activeMenu }) => {
 
   return (
     <div
-    className="flex gap-5 bg-white border-b border-gray-200/50 backdrop-blur-[2px] py-4
+      className="flex gap-5 bg-white border-b border-gray-200/50 backdrop-blur-[2px] py-4
     px-7 sticky top-0 z-30
     "
     >
       <button
-        className=""
+        className="block lg:hidden text-black"
         onClick={() => {
           setOpenSideMenu(!openSideMenu);
         }}
       >
         {openSideMenu ? (
-          <HiOutlineX className="" />
+          <HiOutlineX className="text-2xl" />
         ) : (
-          <HiOutlineMenu className="" />
+          <HiOutlineMenu className="text-2xl" />
         )}
       </button>
 
-      <h2 className="">Expense-Tracker</h2>
+      <h2
+        className="
+      text-lg font-medium text-black"
+      >
+        Expense-Tracker
+      </h2>
       {openSideMenu && (
-        <div className="">
+        <div className="fixed top-[61px] -ml-4 bg-white">
           <SideMenu activeMenu={activeMenu} />
         </div>
       )}
--- frontend/expense-tracker/src/Layout/SideMenu.jsx
+++ frontend/expense-tracker/src/Layout/SideMenu.jsx
@@ -1,6 +1,26 @@
-import React from "react";
+import React, { useContext } from "react";
+import { SIDE_MENU_DATA } from "../utils/data";
+import { UserContext } from "../context/Context";
+import { useNavigate } from "react-router-dom";
 // After (The Fix)
-export function SideMenu() {
+export function SideMenu({ activeMenu }) {
+  const { user, clearUser } = useContext(UserContext);
+
+  const naviage = useNaviage();
+  const handleClick = (route) => {
+    if (route === "logout") {
+      handleLogout();
+      return;
+    }
+
+    navigator(route);
+  };
+
+  const handleLogout = () => {
+    localStorage.clear();
+    clearUser();
+    naviage("/login");
+  };
   return (
     <>
       <div>SideBar!</div>
