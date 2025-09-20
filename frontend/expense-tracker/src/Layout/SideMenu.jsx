1facbc1b901745875bbda237275610905562d521
KrishDevCrafting <krishkumer150@gmail.com>
2025-09-20 15:46:14 +0530

    updated

--- frontend/expense-tracker/src/Layout/SideMenu.jsx
+++ frontend/expense-tracker/src/Layout/SideMenu.jsx
@@ -2,28 +2,60 @@ import React, { useContext } from "react";
 import { SIDE_MENU_DATA } from "../utils/data";
 import { UserContext } from "../context/Context";
 import { useNavigate } from "react-router-dom";
-// After (The Fix)
+
 export function SideMenu({ activeMenu }) {
   const { user, clearUser } = useContext(UserContext);
+  const navigate = useNavigate();
 
-  const naviage = useNaviage();
   const handleClick = (route) => {
     if (route === "logout") {
       handleLogout();
       return;
     }
 
-    navigator(route);
+    navigate(route);
   };
 
   const handleLogout = () => {
     localStorage.clear();
     clearUser();
-    naviage("/login");
+    navigate("/login");
   };
+
   return (
-    <>
-      <div>SideBar!</div>
-    </>
+    <div
+      className="w-64 h-[calc(100vh-61px)]
+    bg-white  border-r border-gray-200/50 p-5 sticky top-[61px] z-20
+    "
+    >
+      <div className="">
+        {user?.profileImageUrl && (
+          <img src={user.profileImageUrl} alt="Profile Image" className="" />
+        )}
+        <h5 className="">{user?.fullName || ""}</h5>
+      </div>
+      {SIDE_MENU_DATA.map((item, index) => (
+        <button
+          key={`menu_${index}`}
+          className={`w-full flex items-center gap-4 text-[15px] ${
+            activeMenu === item.label ? "text-white bg-primary" : ""
+          } py-3 px-6 rounded-lg mb-3`}
+          onClick={() => handleClick(item.path)}
+        >
+          <item.icon className="" />
+        </button>
+      ))}
+    </div>
   );
 }
+
+
+
+
+
+// // {
+//     "fullName": "Krish.K",
+//     "password": "Krish2004",
+//     "email": "Krish@gmail.com",
+//     "profileImage": "http://localhost:"
+// // }
