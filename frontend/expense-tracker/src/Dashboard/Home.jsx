d60686ed4b8458a9f2ab1ef3bdb3c62afd16f8ee
KrishDevCrafting <krishkumer150@gmail.com>
2025-09-25 21:45:58 +0530

    SideMenui

--- frontend/expense-tracker/src/Dashboard/Home.jsx
+++ frontend/expense-tracker/src/Dashboard/Home.jsx
@@ -1,13 +1,41 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { DashboardLayout } from "../Layout/DashboardLayout";
 import { useUserAuth } from "../hooks/useUserAuth";
+import axiosInstance from "../utils/axios";
+import { API_PATHS } from "../utils/apiPaths";
 
 export default function Home() {
   useUserAuth();
+
+  const [dashboardData, setDashboardData] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  const fetchDashboard = async () => {
+    if (loading) return;
+    setLoading(true);
+
+    try {
+      const response = await axiosInstance.get(API_PATHS.DASHBOARD.GET_DATA);
+      if (response.data) {
+        setDashboardData(response.data);
+      }
+    } catch (error) {
+      console.log("Something went wrong. Please try again!", error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchDashboard();
+    // eslint-disable-next-line
+  }, []);
+
   return (
     <DashboardLayout activeMenu="Dashboard">
       <div className="my-5 mx-auto">
         <h1 className="text-xl font-bold">Welcome to Dashboard</h1>
+        {/* You can render dashboardData here */}
       </div>
     </DashboardLayout>
   );
