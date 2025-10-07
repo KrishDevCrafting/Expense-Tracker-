042d22bae2fd272a9cf20636f826be87f90e257a
KrishDevCrafting <krishkumer150@gmail.com>
2025-10-07 23:04:40 +0530

    text-updated

--- frontend/expense-tracker/src/components/Chats/customPiechart.jsx
+++ frontend/expense-tracker/src/components/Chats/customPiechart.jsx
@@ -14,5 +14,40 @@ export const CustomPiechart = ({
   totalAmount,
   showTextAnchor,
 }) => {
-  return <div>customPiechart</div>;
+  return <div>
+
+
+
+    <ResponsiveContainer width="100%"
+    height={380}
+    <PieChart>
+    
+    <Pie data={data}
+dataKey="amount"
+nameKey="name"
+cx="50%"
+cy="50%"
+outerRadius={100}
+labelline={false}
+>
+{data.map((entry,index)=>(
+  <Cell key={`cell-${index}`} fill={color[index%colors.length]}/>
+))}
+
+</Pie>
+    <Tooltip/>
+    <Legend/>
+    {showTextAnchor &&
+    (
+      <>
+      
+      <text x="50%"
+      y="50%"
+      dy={-25}
+      textAnchor="middle"
+      fill="#666"
+      fontSize="14px"></text>
+      </>
+    )}
+  </div>;
 };
