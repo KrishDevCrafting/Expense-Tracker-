09d589369c34ada45bc76802ff4c4445ef2d5c56
KrishDevCrafting <krishkumer150@gmail.com>
2025-10-07 22:48:41 +0530

    En France

--- frontend/expense-tracker/src/components/Chats/customPiechart.jsx
+++ frontend/expense-tracker/src/components/Chats/customPiechart.jsx
@@ -1,5 +1,12 @@
 import React from "react";
-
+import {
+  PieChart,
+  Pie,
+  Cell,
+  Tooltip,
+  ResponsiveContainer,
+  Legend,
+} from "recharts";
 export const CustomPiechart = ({
   data,
   label,
