4ac6c7f22eee677a371362b3b80700a4235ac694
KrishDevCrafting <krishkumer150@gmail.com>
2025-10-10 15:03:12 +0530

    expense

--- frontend/expense-tracker/src/components/Dashboard/ExpenseTransaction.jsx
+++ frontend/expense-tracker/src/components/Dashboard/ExpenseTransaction.jsx
@@ -1,18 +1,34 @@
 import React from "react";
 import { LuArrowRight } from "react-icons/lu";
+import { TransactionInfoCard } from "../Card/TransactionInfoCard";
+import moment from "moment";
 
-export const ExpenseTransaction = ({ transaction, OnseeMore }) => {
+export const ExpenseTransaction = ({ transaction = [], onSeeMore }) => {
   return (
     <div className="card">
       <div className="flex items-center justify-between">
         <h5 className="text-lg">Expense</h5>
         <button
           className="card-btn flex items-center gap-1"
-          onClick={OnseeMore}
+          onClick={onSeeMore}
         >
           See All
           <LuArrowRight className="text-base" />
         </button>
+      </div>
+
+      <div className="mt-6">
+        {transaction.slice(0, 5).map((expense) => (
+          <TransactionInfoCard
+            key={expense._id}
+            title={expense.category}
+            icon={expense.icon}
+            data={moment(expense.date).format("Do MMM YYYY")}
+            amount={expense.amount}
+            type="expense"
+            hideDeleteBtn
+          />
+        ))}
       </div>
     </div>
   );
