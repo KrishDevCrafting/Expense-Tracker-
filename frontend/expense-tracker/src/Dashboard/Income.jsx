53ed06139634882990cde0080af74a7ba0363b9b
KrishDevCrafting <krishkumer150@gmail.com>
2025-10-17 19:59:52 +0530

    updated

--- frontend/expense-tracker/src/Dashboard/Income.jsx
+++ frontend/expense-tracker/src/Dashboard/Income.jsx
@@ -1,9 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import { DashboardLayout } from "../Layout/DashboardLayout";
+
+
+
 export default function Income() {
+
+const [openAddIncomeModal,setOpenIncomeModal] = useState(false)
+
   return (
     <DashboardLayout activeMenu="Income">
-      <div className="my-5 mx-auto"></div>
+      <div className="my-5 mx-auto">
+        <div className="grid grid-cols-1 gap-6">
+          <div className="">
+            <IncomeOverview
+              transactions={incomeData}
+              onAddIncome={() => setOpenIncomeModal(true)}
+            />
+          </div>
+        </div>
+      </div>
     </DashboardLayout>
   );
 }
