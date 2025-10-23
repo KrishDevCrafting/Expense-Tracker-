3cf54d60495d74ff195d0484c4be8ffa168cbfc4
KrishDevCrafting <krishkumer150@gmail.com>
2025-10-23 13:54:26 +0530

    income-propes

--- frontend/expense-tracker/src/components/layout/Modal.jsx
+++ frontend/expense-tracker/src/components/layout/Modal.jsx
@@ -3,14 +3,32 @@ import React from "react";
 const Modal = ({ Childern, isOpen, onClose, title }) => {
   return (
     <>
-      <div></div>
-      <div></div>
-      <div>
+      <div className="fixed top-0 right-0 left-0 z-50 flex justify-center item-center w-full h-[calc(100%-1rem)] max-h-full overflow-y-autoauto overflow-x-hidden bg-black/20 bg-opacity-50">
+        <div className="relative p-4 w-full max-w-2xl max-h-full">
+          <div className="relative bg-white rounded-lg shadow-sm dark:bg-gray-700">
+            <div className="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600 border-gray-200">
+              <h3 className="text-lg font-medium text-gray-900 dark:text-white">
+                {title}
+              </h3>
+              <button
+                type="button"
+                className="text-gray-400 bg-transparent hover:text-gray-900 rounded-lg text-sm w-8 h-8 inline-flex justify-center item-center darrk:hover:bg-gray-600 dark:hover:text-white cursor-pointed"
+                onClick={onClose}
+              >
+                X
+              </button>
+            </div>
+            {/* {Modal body} */}
 
-<h3
-className=""
->{title}</h3>
-
+            <div
+              className="
+            p-4 md:p-5 space-y-4
+            "
+            >
+              {Childern}
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
