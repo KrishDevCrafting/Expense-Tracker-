fc8411cf5e063f5ba09b098e32b11511ad3c9704
KrishDevCrafting <krishkumer150@gmail.com>
2025-10-27 16:12:21 +0530

    updated

--- frontend/expense-tracker/src/components/EmojiPickerPopup.jsx
+++ frontend/expense-tracker/src/components/EmojiPickerPopup.jsx
@@ -4,7 +4,29 @@ import { LuImage, Lux } from "react-icons/lu";
 const EmojiPickerPopup = ({ icon, onSelect }) => {
   const [isOpen, setIsOpen] = useState(false);
 
-  return <div>EmojiPickerPopup</div>;
+  return (
+    <div className="">
+      <div className="" onClick={() => setIsOpen(true)}>
+        <div className="">
+          {icon ? <img src={icon} alt="Icon" className="" /> : <LuImage />}
+        </div>
+
+        <p className="">{icon ? "Change Icon" : "Pick up"}</p>
+      </div>
+
+      {isOpen && (
+        <div className="">
+          <button className="" onClick={() => setIsOpen(false)}>
+            <Lux />
+          </button>
+          <EmojiPicker
+            open={isOpen}
+            onEmojiClick={(emoji) => onselect(emoji?.imageUrl || "")}
+          />
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default EmojiPickerPopup;
