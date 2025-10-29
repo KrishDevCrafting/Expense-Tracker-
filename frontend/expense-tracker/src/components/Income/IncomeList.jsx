ea22dc5381d7e210879870f05087058b3a24672a
KrishDevCrafting <krishkumer150@gmail.com>
2025-10-29 22:56:13 +0530

    updated

--- frontend/expense-tracker/src/components/Income/IncomeList.jsx
+++ frontend/expense-tracker/src/components/Income/IncomeList.jsx
@@ -1,11 +1,22 @@
-import React from 'react'
+import React from "react";
+import { LuDownload } from "react-icons/lu";
 
-const IncomeList = ({
-    trasactions,onDelete,onDownload
-}) => {
+const IncomeList = ({ trasactions, onDelete, onDownload }) => {
   return (
-    <div>IncomeList</div>
-  )
-}
+    <>
+      <div>IncomeList</div>
 
-export default IncomeList
+      <div className="card">
+        <div className="flex item-center justify-between">
+          <h5 className="text-lg">Income Source</h5>
+          <button className="" onclick={onDownload}>
+            <LuDownload className="text-base" />
+            Download
+          </button>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default IncomeList;
