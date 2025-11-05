0a61a42bd973837107266e85bbf5805827fee1b0
KrishDevCrafting <krishkumer150@gmail.com>
2025-11-05 21:28:45 +0530

    expense-updated

--- frontend/expense-tracker/src/components/Expense/ExpenseOverview.jsx
+++ frontend/expense-tracker/src/components/Expense/ExpenseOverview.jsx
@@ -14,16 +14,16 @@ const ExpenseOverview = ({ transactions, onExpenseIncome }) => {
   return (
     <>
       <div className="bg-white p-6 rounded-2xl shadow-gray-100 border-gray-200/50">
-        <div className="">
+        <div className="flex item-centerjustify-between">
           <div className="">
             <h5>Expense Overview</h5>
-            <p className="">
+            <p className="text-xs text-gray-400 mt-0.5">
               Track your spending trends over time and gain insights your money
               goes.
             </p>
           </div>
           <button className="" onClick={onExpenseIncome}>
-            <LuPlug className="" />
+            <LuPlug className="text-lg" />
             Add Expense
           </button>
         </div>
