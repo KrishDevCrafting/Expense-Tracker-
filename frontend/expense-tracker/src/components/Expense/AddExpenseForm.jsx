5b0e4aeefdf69363e55b55f3e4f297e8a4ccbab1
KrishDevCrafting <krishkumer150@gmail.com>
2025-11-09 22:15:00 +0530

    updated

--- frontend/expense-tracker/src/components/Expense/AddExpenseForm.jsx
+++ frontend/expense-tracker/src/components/Expense/AddExpenseForm.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React, { useState } from "react";
+import Input from "../Inputs/Inputs";
+import EmojiPicker from "emoji-picker-react";
+const AddExpenseForm = ({ onAddExpense }) => {
+  const [Income, setIncome] = useState({
+    category: "",
+    amount: "",
+    date: "",
+    icon: "",
+  });
 
-const AddExpenseForm = ({onAddExpense}) => {
-  return (
-    <div>AddExpenseForm</div>
-  )
-}
+  const handleChange = (key, value) =>
+    setIncome({
+      ...Income,
+      [key]: value,
+    });
+  return <div>AddExpenseForm</div>;
+};
 
-export default AddExpenseForm
+export default AddExpenseForm;
