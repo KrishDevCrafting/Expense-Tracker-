cdbf652cba5ddf1ddcf852ffb07148c67d90ec12
KrishDevCrafting <krishkumer150@gmail.com>
2025-11-11 21:54:32 +0530

    Add-ExpenseUpdated

--- frontend/expense-tracker/src/components/Expense/AddExpenseForm.jsx
+++ frontend/expense-tracker/src/components/Expense/AddExpenseForm.jsx
@@ -14,7 +14,44 @@ const AddExpenseForm = ({ onAddExpense }) => {
       ...Income,
       [key]: value,
     });
-  return <div>AddExpenseForm</div>;
+  return (
+    <div>
+      <EmojiPickerPopup
+        icon={Income.icon}
+        onSelect={(SelectedIcon) => handleChange("icon", SelectedIcon)}
+      />
+
+      <Input
+        value={Income.category}
+        onChange={({ target }) => handleChange("category", target.value)}
+        label="Category"
+        placeholder="Rent,Groceries,etc."
+        type="text"
+      />
+
+      <Input
+        value={Income.amount}
+        onChange={({ target }) => handleChange("amount", target.value)}
+        label="Amount"
+        placeholder=""
+        type="number"
+      />
+
+      <Input
+        value={Income.date}
+        onChange={({ target }) => handleChange("date", target.value)}
+        label="Date"
+        placeholder=""
+        type="date"
+      />
+
+      <div>
+        <button type="button" onClick={() => onAddExpense(Income)} className="">
+          Add Expense
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default AddExpenseForm;
