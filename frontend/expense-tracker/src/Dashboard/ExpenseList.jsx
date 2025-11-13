b9f6e5c12c630b6e2496bd252360cc492228ce04
KrishDevCrafting <krishkumer150@gmail.com>
2025-11-13 21:58:11 +0530

    updated

--- frontend/expense-tracker/src/Dashboard/ExpenseList.jsx
+++ frontend/expense-tracker/src/Dashboard/ExpenseList.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const ExpenseList = ({ transactions, onDelete, onDownload }) => {
-  return <div>ExpenseList</div>;
+  return <div className="card"></div>;
 };
 
 export default ExpenseList;
