3ca779b512ea6fd5789f2adcefc2a7c13e2cdb40
Krish <krishkumer150@gmail.com>
2025-11-14 00:10:01 +0530

    expense-tracker

--- frontend/expense-tracker/src/Dashboard/ExpenseList.jsx
+++ frontend/expense-tracker/src/Dashboard/ExpenseList.jsx
@@ -1,7 +1,8 @@
 import moment from "moment";
 import React from "react";
 import { LuDownload } from "react-icons/lu";
-import TrasactionInfo from "../components/Transaction/TrasactionInfo";
+import { TransactionInfoCard } from "../components/Card/TransactionInfoCard";
+
 const ExpenseList = ({ transactions, onDelete, onDownload }) => {
   return (
     <div className="card">
@@ -15,7 +16,7 @@ const ExpenseList = ({ transactions, onDelete, onDownload }) => {
 
       <div className="grid grid-cols-1 md:grid-cols-2">
         {transactions?.map((expense) => {
-          <TrasactionInfo
+          <TransactionInfoCard
             key={expense._id}
             title={expense.catogory}
             icon={expense.icon}
