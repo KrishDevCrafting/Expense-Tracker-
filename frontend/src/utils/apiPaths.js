098e52cddc8102ba8a0a06c60352ee33e6253891
KrishDevCrafting <krishkumer150@gmail.com>
2025-12-08 14:48:10 +0530

    updated

--- frontend/src/utils/apiPaths.js
+++ frontend/src/utils/apiPaths.js
@@ -1,34 +1,29 @@
-export const BASE_URL = "http://localhost:8000";
-
+export const BASE_URL = "https://expense-tracker-1-f3y8.onrender.com/";
 
 // utils/apiPath.js
-
+agd;
 export const API_PATHS = {
-    AUTH:{
-        LOGIN: "/api/auth/login",
-        REGISTER: "/api/auth/register",
-        GET_USER_INFO: "/api/auth/getuser"
-    },
-    DASHBOARD:{
-        GET_DATA:"/api/dashboard"
-    },
-    INCOME:{
-        ADD_INCOME:"/api/income/add",
-        GET_ALL_INCOME: "/api/income/get",
-        DELETE_INCOME: (incomeId) => `/api/income/${
-            incomeId
-        }`,
-        DOWNLOAD_INCOME: "/api/income/download",
-    },
-    EXPENSE:{
-        ADD_EXPENSE:"/api/expense/add",
-        GET_ALL_EXPENSE: "/api/expense/get",
-        DELETE_EXPENSE: (expenseId) => `/api/expense/${
-            expenseId
-        }`,
-        DOWNLOAD_EXPENSE: "/api/expense/download",
-    },
-    IMAGE:{
-        UPLOAD_IMAGE: "/api/auth/upload-image",
-    },
-}
+  AUTH: {
+    LOGIN: "/api/auth/login",
+    REGISTER: "/api/auth/register",
+    GET_USER_INFO: "/api/auth/getuser",
+  },
+  DASHBOARD: {
+    GET_DATA: "/api/dashboard",
+  },
+  INCOME: {
+    ADD_INCOME: "/api/income/add",
+    GET_ALL_INCOME: "/api/income/get",
+    DELETE_INCOME: (incomeId) => `/api/income/${incomeId}`,
+    DOWNLOAD_INCOME: "/api/income/download",
+  },
+  EXPENSE: {
+    ADD_EXPENSE: "/api/expense/add",
+    GET_ALL_EXPENSE: "/api/expense/get",
+    DELETE_EXPENSE: (expenseId) => `/api/expense/${expenseId}`,
+    DOWNLOAD_EXPENSE: "/api/expense/download",
+  },
+  IMAGE: {
+    UPLOAD_IMAGE: "/api/auth/upload-image",
+  },
+};
